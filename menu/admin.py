303da9d4b375ac719500f912276ccd8e984115d1
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-23 14:46:58 -0500

    feat: Implement MenuCategory and MenuItem models with admin registration

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from menu.models import MenuCategory, MenuItem
+
+
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    pass
