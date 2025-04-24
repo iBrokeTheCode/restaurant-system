e0c22b35eb475aa0a315756797d35d6c2678815b
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-24 10:31:15 -0500

    feat: Implement table management views and templates for create, update, delete, and list functionalities

--- project/urls.py
+++ project/urls.py
@@ -3,15 +3,16 @@ from django.contrib import admin
 from django.urls import include, path
 
 DJANGO_URLS = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
 THIRD_PARTY_URLS = [] + debug_toolbar_urls()
 
 
 LOCAL_URLS = [
-    path("", include("core.urls", namespace="core")),
-    path("menu/", include("menu.urls", namespace="menu")),
+    path('', include('core.urls', namespace='core')),
+    path('menu/', include('menu.urls', namespace='menu')),
+    path('tables/', include('tables.urls', namespace='tables')),
 ]
 
 urlpatterns = DJANGO_URLS + THIRD_PARTY_URLS + LOCAL_URLS
--- tables/views.py
+++ tables/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-# Create your views here.
+from tables.models import Table
+
+
+class TableListView(ListView):
+    model = Table
+    template_name = 'tables/table_list.html'
+    context_object_name = 'tables'
+
+
+class TableDetailView(DetailView):
+    model = Table
+    template_name = 'tables/table_detail.html'
+    context_object_name = 'table'  # Default: object
+
+
+class TableCreateView(CreateView):
+    model = Table
+    fields = ('table_number', 'seats', 'status')
+    template_name = 'tables/table_form.html'
+    success_url = reverse_lazy('tables:table-list')
+    context_object_name = 'table'  # Default: view.object
+
+
+class TableUpdateView(UpdateView):
+    model = Table
+    fields = ('table_number', 'seats', 'status')
+    template_name = 'tables/table_form.html'
+    success_url = reverse_lazy('tables:table-list')
+    context_object_name = 'table'  # Default: view.object
+
+
+class TableDeleteView(DeleteView):
+    model = Table
+    template_name = 'tables/table_confirm_delete.html'
+    success_url = reverse_lazy('tables:table-list')
+    context_object_name = 'table'  # Default: object
