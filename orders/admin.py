d32cacfa65a7a899279acb44e309b02ce0590b40
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-23 20:53:44 -0500

    feat: Add action to mark selected orders as served in Order admin interface

--- orders/admin.py
+++ orders/admin.py
@@ -9,6 +9,11 @@ class OrderItemInline(admin.TabularInline):
     readonly_fields = ('total_price',)
 
 
+@admin.action(description='Mark selected orders as served')
+def mark_as_served(modeladmin, request, queryset):
+    queryset.update(status=Order.OrderStatusChoices.SERVED)
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'table', 'status', 'created_at')
@@ -16,3 +21,4 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = ('table__table_number',)
     readonly_fields = ('created_at', 'updated_at')
     inlines = [OrderItemInline]
+    actions = [mark_as_served]
