0460ca41b5ea783e803384889eb189f92384cfbc
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-24 15:38:20 -0500

    feat: Implement order management views and templates for listing, detail, creation, update, and deletion of orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-# Create your views here.
+from orders.models import Order
+
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'orders/order_list.html'
+    context_object_name = 'orders'
+
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'orders/order_detail.html'
+    context_object_name = 'order'
+
+
+class OrderCreateView(CreateView):
+    pass
+
+
+class OrderUpdateView(UpdateView):
+    pass
+
+
+class OrderDeleteView(DeleteView):
+    model = Order
+    template_name = 'orders/order_confirm_delete.html'
+    success_url = reverse_lazy('orders:order-list')
