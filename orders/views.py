924e026bc2f6fdbe03e6b18f1a8c60fe29f38231
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-24 16:47:23 -0500

    feat: Implement order creation and update views with inline formset for order items

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,5 @@
+from django.forms.models import inlineformset_factory
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
@@ -7,7 +9,15 @@ from django.views.generic import (
     UpdateView,
 )
 
-from orders.models import Order
+from orders.models import Order, OrderItem
+
+OrderItemFormSet = inlineformset_factory(
+    parent_model=Order,
+    model=OrderItem,
+    fields=('menu_item', 'quantity', 'unit_price', 'note'),
+    extra=1,
+    can_delete=True,
+)
 
 
 class OrderListView(ListView):
@@ -23,11 +33,72 @@ class OrderDetailView(DetailView):
 
 
 class OrderCreateView(CreateView):
-    pass
+    model = Order
+    fields = ('table', 'status')
+    template_name = 'orders/order_form.html'
+    context_object_name = 'order'
+    success_url = reverse_lazy('orders:order-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.POST:
+            context['formset'] = OrderItemFormSet(self.request.POST)
+        else:
+            context['formset'] = OrderItemFormSet()
+
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+
+        if formset.is_valid():
+            # Save order
+            self.object = form.save()
+            # Assign it as the parent of order items formset
+            formset.instance = self.object
+            # Save formset
+            formset.save()
+            return redirect(self.success_url)
+        else:
+            # re-render the form with errors.
+            return self.render_to_response(self.get_context_data(form=form))
 
 
 class OrderUpdateView(UpdateView):
-    pass
+    model = Order
+    fields = ('table', 'status')
+    template_name = 'orders/order_form.html'
+    context_object_name = 'order'
+    success_url = reverse_lazy('orders:order-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.POST:
+            context['formset'] = OrderItemFormSet(
+                self.request.POST, instance=self.get_object()
+            )
+        else:
+            context['formset'] = OrderItemFormSet(instance=self.get_object())
+
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+
+# TODO: Go back to previous page (use next url param)
 
 
 class OrderDeleteView(DeleteView):
