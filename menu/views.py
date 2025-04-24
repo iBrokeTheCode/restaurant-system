33aa53169e6820e5577dc011a3cbb3210f5c8a13
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-24 15:04:47 -0500

    feat: Add menu item detail view and update related templates for improved item management

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,11 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
 from menu.models import MenuItem
 
@@ -10,12 +16,23 @@ class MenuItemListView(ListView):
     context_object_name = 'menu_items'  # Default menuitem_list
 
 
+class MenuItemDetailView(DetailView):
+    model = MenuItem
+    template_name = 'menu/menu_item_detail.html'
+    context_object_name = 'menu_item'
+
+
 class MenuItemCreateView(CreateView):
     model = MenuItem
     fields = ('name', 'description', 'price', 'category')
     template_name = 'menu/menu_item_form.html'
     success_url = reverse_lazy('menu:menu-item-list')
     context_object_name = 'menu_item'  # Default: view.object
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', self.success_url)
+        return context
 
 
 class MenuItemUpdateView(UpdateView):
@@ -24,6 +41,15 @@ class MenuItemUpdateView(UpdateView):
     template_name = 'menu/menu_item_form.html'
     success_url = reverse_lazy('menu:menu-item-list')
     context_object_name = 'menu_item'  # Default view.object
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', self.success_url)
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        return next_url if next_url else str(self.success_url)
 
 
 class MenuItemDeleteView(DeleteView):
