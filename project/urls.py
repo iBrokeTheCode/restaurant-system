11bf858534817194e7c1d6946e5da4c4bb919075
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-24 05:56:44 -0500

    feat: Add core app URLs and views for home page

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
-urlpatterns = [
+DJANGO_URLS = [
     path('admin/', admin.site.urls),
-] + debug_toolbar_urls()
+]
+
+THIRD_PARTY_URLS = [] + debug_toolbar_urls()
+
+
+LOCAL_URLS = [
+    path('', include('core.urls', namespace='core')),
+]
+
+urlpatterns = DJANGO_URLS + THIRD_PARTY_URLS + LOCAL_URLS
