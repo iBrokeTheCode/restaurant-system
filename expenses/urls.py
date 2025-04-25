7f44aa6f6a3f9a61896cbb2413c99624b9dc0f7e
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-25 07:11:49 -0500

    feat: Implement expense update functionality with redirect support and create expense form

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,6 +5,7 @@ from expenses.views import (
     ExpenseDeleteView,
     ExpenseDetailView,
     ExpenseListView,
+    ExpenseUpdateView,
 )
 
 app_name = 'expenses'
@@ -13,5 +14,6 @@ urlpatterns = [
     path('', ExpenseListView.as_view(), name='expense-list'),
     path('<int:pk>/', ExpenseDetailView.as_view(), name='expense-detail'),
     path('create/', ExpenseCreateView.as_view(), name='expense-create'),
+    path('<int:pk>/update/', ExpenseUpdateView.as_view(), name='expense-update'),
     path('<int:pk>/delete/', ExpenseDeleteView.as_view(), name='expense-delete'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,11 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, DetailView, ListView
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
 from expenses.models import Expense
 
@@ -16,16 +22,40 @@ class ExpenseDetailView(DetailView):
     context_object_name = 'expense'
 
 
-class ExpenseDeleteView(DeleteView):
-    model = Expense
-    template_name = 'expenses/expense_confirm_delete.html'
-    context_object_name = 'expense'
-    success_url = reverse_lazy('expenses:expense-list')
-
-
 class ExpenseCreateView(CreateView):
     model = Expense
     fields = ('amount', 'description', 'category', 'date')
     template_name = 'expenses/expense_form.html'
     success_url = reverse_lazy('expenses:expense-list')
     context_object_name = 'expense'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', self.success_url)
+
+        return context
+
+
+class ExpenseUpdateView(UpdateView):
+    model = Expense
+    fields = ('amount', 'description', 'category', 'date')
+    template_name = 'expenses/expense_form.html'
+    success_url = reverse_lazy('expenses:expense-list')
+    context_object_name = 'expense'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', self.success_url)
+
+        return context
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        return next_url if next_url else super().get_success_url()
+
+
+class ExpenseDeleteView(DeleteView):
+    model = Expense
+    template_name = 'expenses/expense_confirm_delete.html'
+    context_object_name = 'expense'
+    success_url = reverse_lazy('expenses:expense-list')
