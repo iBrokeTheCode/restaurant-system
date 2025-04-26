e5db0e1dc7a5f4ed76c5e2a8b3080664b2c1eaf2
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-26 05:40:51 -0500

    feat: Enhance authentication flow with success messages on login and logout; enforce login requirement for dashboard

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect, render
 
@@ -7,6 +9,7 @@ def home(request):
     return render(request, 'core/home.html')
 
 
+@login_required()
 def dashboard(request):
     return render(request, 'core/dashboard.html')
 
@@ -15,7 +18,12 @@ class CustomLoginView(LoginView):
     template_name = 'core/login.html'
     redirect_authenticated_user = True
 
+    def form_valid(self, form):
+        messages.success(self.request, 'You have successfully logged in!')
+        return super().form_valid(form)
+
 
 def logout_view(request):
     logout(request)
+    messages.success(request, 'You have been logged out successfully!')
     return redirect('core:home')
