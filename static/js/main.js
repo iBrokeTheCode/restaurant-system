8e5a297748700c7ef6e434bb78adff802a6d6076
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-26 17:12:08 -0500

    feat: Wrap content in sections for day menu and tables status; update navbar for scroll effect

--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,15 @@ setTimeout(() => {
     alert.remove();
   }
 }, 3000);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("mainNav");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+});
