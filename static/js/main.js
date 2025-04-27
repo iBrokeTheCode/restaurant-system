35c04071704b7e3f323f9672076fab1a017fcd24
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-27 12:06:28 -0500

    feat: Update navbar styling and adjust section layouts for improved visibility and consistency

--- static/js/main.js
+++ static/js/main.js
@@ -5,15 +5,3 @@ setTimeout(() => {
     alert.remove();
   }
 }, 3000);
-
-document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.getElementById("mainNav");
-
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 0) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
-});
