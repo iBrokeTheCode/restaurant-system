6ce6c5373f70461123e0d3b2677d07543bc99dc3
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-04-29 08:32:20 -0500

    feat: Add media URL and root settings for file uploads

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,6 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -19,3 +21,6 @@ LOCAL_URLS = [
 ]
 
 urlpatterns = DJANGO_URLS + THIRD_PARTY_URLS + LOCAL_URLS
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
