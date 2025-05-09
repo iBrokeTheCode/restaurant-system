0cadfdfea33b1799de3848b638346b7f7ea827bd
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-05-09 08:09:39 -0500

    chore: Add navigation inside expenses templates keeping filtered list

--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,6 @@
-from calendar import monthrange
-
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.utils.timezone import now
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -12,31 +9,35 @@ from django.views.generic import (
     UpdateView,
 )
 
-from core.mixins import GroupRequiredMixin
+from core.mixins import DateRangeFilterMixin, GroupRequiredMixin
 from expenses.forms import ExpenseForm
 from expenses.models import Expense
 
 
-class ExpenseListView(LoginRequiredMixin, GroupRequiredMixin, ListView):
+class ExpenseListView(
+    LoginRequiredMixin, GroupRequiredMixin, DateRangeFilterMixin, ListView
+):
     model = Expense
     template_name = 'expenses/expense_list.html'
     context_object_name = 'expenses'
     group_required = ['Owner']
     raise_exception = True
+    date_field = 'date'
 
     def get_queryset(self):
-        """Filter expense for the current month."""
+        """Filter sales for the selected date range."""
         qs = super().get_queryset()
-        today = now().date()
-        first_day = today.replace(day=1)
-        last_day = today.replace(day=monthrange(today.year, today.month)[1])
-        return qs.filter(date__range=(first_day, last_day))
+        return self.filter_queryset_by_date(qs)
 
     def get_context_data(self, **kwargs):
-        """Override method to pass date in the context"""
-        ctx = super().get_context_data(**kwargs)
-        ctx['date'] = now().date()
-        return ctx
+        """Override method to pass date in the context."""
+        self.object_list = self.get_queryset()
+        context = super().get_context_data(**kwargs)
+        start, end = self.get_date_range()
+        date = start if start == end else f'{start} to {end}'
+
+        context.update({'date': date})
+        return context
 
 
 class ExpenseDetailView(LoginRequiredMixin, GroupRequiredMixin, DetailView):
@@ -45,6 +46,15 @@ class ExpenseDetailView(LoginRequiredMixin, GroupRequiredMixin, DetailView):
     context_object_name = 'expense'
     group_required = ['Owner']
     raise_exception = True
+
+    def get_context_data(self, **kwargs):
+        """Return to the previous filtered page or the default page."""
+        context = super().get_context_data(**kwargs)
+        context['previous'] = self.request.GET.get(
+            'previous', reverse_lazy('expenses:expense-list')
+        )
+
+        return context
 
 
 class ExpenseCreateView(LoginRequiredMixin, GroupRequiredMixin, CreateView):
@@ -59,7 +69,6 @@ class ExpenseCreateView(LoginRequiredMixin, GroupRequiredMixin, CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['next'] = self.request.GET.get('next', self.success_url)
-
         return context
 
     def form_valid(self, form):
@@ -79,7 +88,6 @@ class ExpenseUpdateView(LoginRequiredMixin, GroupRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['next'] = self.request.GET.get('next', self.success_url)
-
         return context
 
     def get_success_url(self):
@@ -98,6 +106,11 @@ class ExpenseDeleteView(LoginRequiredMixin, GroupRequiredMixin, DeleteView):
     success_url = reverse_lazy('expenses:expense-list')
     group_required = ['Owner']
     raise_exception = True
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', self.success_url)
+        return context
 
     def post(self, request, *args, **kwargs):
         return self.delete(request, *args, **kwargs)
--- sales/views.py
+++ sales/views.py
@@ -134,7 +134,6 @@ class SaleDeleteView(LoginRequiredMixin, GroupRequiredMixin, DeleteView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['next'] = self.request.GET.get('next', self.success_url)
-
         return context
 
     def delete(self, request, *args, **kwargs):
